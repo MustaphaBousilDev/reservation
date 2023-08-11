2b6bf22f312001d8e0cc540e64e09ed6fe7df979
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-08-11 20:08:08 +0100

    finish by adding Joi validation scheme and starting creating abstract repository

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigService } from '@nestjs/config'; 
+import { ConfigService } from '@nestjs/config';
 import { ConfigModule } from '../config/config.module';
 
 @Module({
